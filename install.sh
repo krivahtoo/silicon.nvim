7536f09e60b439613f212c0f4dec3ffd897f2f77
Noah Too <krivahtoo@gmail.com>
2022-09-12 08:51:12 +0300

    build: added github release workflow

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ cargo build --release
 mkdir -p lua
 
 if [ "$(uname)" == "Darwin" ]; then
-    mv target/release/libsilicon_nvim.dylib lua/silicon.so
+    mv target/release/libsilicon.dylib lua/silicon.so
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    mv target/release/libsilicon_nvim.so lua/silicon.so
+    mv target/release/libsilicon.so lua/silicon.so
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    mv target/release/silicon_nvim.dll lua/silicon.dll
+    mv target/release/silicon.dll lua/silicon.dll
 fi
